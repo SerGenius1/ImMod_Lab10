c6cccb0161497511661a63d24151c142bc2bcbcc
SerGenius1 <s.burnashev03@gmail.com>
2024-06-08 13:33:18 +0700

    Добавьте файлы проекта.

---
+++ Lab10/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.edLamb1 = new System.Windows.Forms.NumericUpDown();
+            this.edLamb2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.edLamb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edLamb2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // edLamb1
+            // 
+            this.edLamb1.DecimalPlaces = 1;
+            this.edLamb1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.edLamb1.Location = new System.Drawing.Point(266, 53);
+            this.edLamb1.Margin = new System.Windows.Forms.Padding(4);
+            this.edLamb1.Name = "edLamb1";
+            this.edLamb1.Size = new System.Drawing.Size(160, 22);
+            this.edLamb1.TabIndex = 1;
+            this.edLamb1.Value = new decimal(new int[] {
+            7,
+            0,
+            0,
+            65536});
+            // 
+            // edLamb2
+            // 
+            this.edLamb2.DecimalPlaces = 1;
+            this.edLamb2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.edLamb2.Location = new System.Drawing.Point(266, 141);
+            this.edLamb2.Margin = new System.Windows.Forms.Padding(4);
+            this.edLamb2.Name = "edLamb2";
+            this.edLamb2.Size = new System.Drawing.Size(160, 22);
+            this.edLamb2.TabIndex = 3;
+            this.edLamb2.Value = new decimal(new int[] {
+            9,
+            0,
+            0,
+            65536});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 143);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(248, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Среднее количестов забитых мячей";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(18, 207);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(408, 32);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Начать матч";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 105);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "2 Команда";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(13, 18);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "1 Команда";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(248, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Среднее количестов забитых мячей";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(14, 270);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Победила команда ...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 336);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.edLamb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.edLamb1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edLamb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edLamb2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown edLamb1;
+        private System.Windows.Forms.NumericUpDown edLamb2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Lab10/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            double lambda1, lambda2;
+            Random rand = new Random();
+
+            lambda1 = (double)edLamb1.Value;
+            lambda2 = (double)edLamb2.Value;
+
+            double a1, a2;
+            double sum1 = 0, sum2 = 0;
+            int goals1 = 0, goals2 = 0;
+
+            while (sum1 >= -lambda1 || sum2 >= -lambda2)
+            {
+                a1 = rand.NextDouble();
+                a2 = rand.NextDouble();
+
+                if (sum1 >= -lambda1)
+                {
+                    sum1 += Math.Log10(a1);
+                    goals1++;
+                }
+
+                if (sum2 >= -lambda2)
+                {
+                    sum2 += Math.Log10(a2);
+                    goals2++;
+                }
+            }
+
+            if (goals1 > goals2)
+            {
+                label5.Text = $"Победила первая команда со счётом {goals1} : {goals2}";
+                return;
+            }
+            else if (goals2 > goals1)
+            {
+                label5.Text = $"Победила вторая команда со счётом {goals1} : {goals2}";
+                return;
+            }
+            else
+            {
+                label5.Text = $"Ничья со счётом {goals1} : {goals2}";
+                return;
+            }
+        }
+    }
+}
